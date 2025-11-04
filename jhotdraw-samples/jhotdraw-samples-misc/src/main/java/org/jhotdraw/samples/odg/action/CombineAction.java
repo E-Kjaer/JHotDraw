93fb8cf769d6b6cdfcdd930a13bdfe19eb6a743d
Emil Kjær Fuglsang <emilkf95@gmail.com>
2025-11-04 15:44:35 +0100

    Actualization - SOLID
    Refactored the code to better follow the SOLID principles

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/odg/action/CombineAction.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/odg/action/CombineAction.java
@@ -53,10 +53,10 @@ public class CombineAction extends GroupAction {
     @Override
     @SuppressWarnings("unchecked")
     public Collection<Figure> ungroupFigures(DrawingView view, CompositeFigure group) {
-        LinkedList<Figure> figures = new LinkedList<Figure>(group.getChildren());
+        LinkedList<Figure> figures = new LinkedList<>(group.getChildren());
         view.clearSelection();
         group.basicRemoveAllChildren();
-        LinkedList<Figure> paths = new LinkedList<Figure>();
+        LinkedList<Figure> paths = new LinkedList<>();
         for (Figure f : figures) {
             ODGPathFigure path = new ODGPathFigure();
             path.removeAllChildren();
