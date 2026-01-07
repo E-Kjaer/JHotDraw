63704ed4dbd2e80c3db2dabc2f4f20a96505cff4
Andreas528 <andreas.runge528@gmail.com>
2026-01-07 20:37:14 +0100

    Changed assert framework to AssertJ

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/scenarios/ellipse/ThenEllipse.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/scenarios/ellipse/ThenEllipse.java
@@ -4,9 +4,11 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import org.jhotdraw.draw.Drawing;
 import org.jhotdraw.draw.figure.EllipseFigure;
-import org.junit.Assert;
+import org.assertj.core.api.Assertions.*;
 
 import java.awt.geom.Rectangle2D;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ThenEllipse extends Stage<ThenEllipse> {
 
@@ -17,12 +19,12 @@ public class ThenEllipse extends Stage<ThenEllipse> {
     EllipseFigure ellipse;
 
     public void an_ellipse_should_exist_in_the_drawing() {
-        Assert.assertTrue(drawing.contains(ellipse));
+        assertThat(drawing.contains(ellipse)).isTrue();
     }
 
     public void the_ellipse_should_have_positive_size() {
         Rectangle2D.Double b = ellipse.getBounds();
-        Assert.assertTrue("Width should be > 0", b.width > 0.0);
-        Assert.assertTrue("Height should be > 0", b.height > 0.0);
+        assertThat(b.width).isPositive();
+        assertThat(b.height).isPositive();
     }
 }
